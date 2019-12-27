8445365f0632ce9364e5a163b4bf93fef11bc091
Cristian Vesa <cristian.vesa150695@gmail.com>
2019-12-27 15:27:13 +0200

    Modified javascript

--- xo-game.js
+++ xo-game.js
@@ -1,6 +1,7 @@
 var origBoard;
-const huPlayer = '0';
+const huPlayer = 'O';
 const aiPlayer = 'X';
+var gameWin = false;
 const winCombos = [
     [0, 1, 2],
     [3, 4, 5],
@@ -18,6 +19,7 @@ startGame();
 function startGame() {
     document.querySelector(".endgame").style.display = "none";
     origBoard = Array.from(Array(9).keys());
+    gameWin = false;
     for (var i = 0; i < cells.length; i++) {
         cells[i].innerText = '';
         cells[i].style.removeProperty('background-color');
@@ -26,5 +28,69 @@ function startGame() {
 }
 
 function turnClick(square) {
-    console.log(square.target.id);
+    if( typeof origBoard[square.target.id] == 'number') {
+        turn(square.target.id, huPlayer);
+        if(!gameWin){
+            if(!checkTie()) {
+                turn(bestSpot(), aiPlayer);
+            }
+        }    
+    }   
+}
+
+function turn(squareId, player) {
+    origBoard[squareId] = player;
+    document.getElementById(squareId).innerText = player;
+    let gameWon = checkWin(origBoard, player);
+    if(gameWon) gameOver(gameWon)
+}
+
+function checkWin(board, player) {
+    let plays = board.reduce((a, e, i) => 
+    (e === player) ? a.concat(i) : a, [])
+    let gameWon = null;
+    for (let [index, win] of winCombos.entries()) {
+        if(win.every(elem => plays.indexOf(elem) > -1)){
+            gameWon = {index: index, player: player};
+            gameWin = true;
+            console.log("Game won:" + gameWon.player);
+            break;
+        }
+    }
+    return gameWon;
+}
+
+function gameOver(gameWon) {
+    for( let index of winCombos[gameWon.index]){
+        document.getElementById(index).style.backgroundColor = gameWon.player == huPlayer ? "blue" : "red";
+    }
+    for (var i = 0; i < cells.length; i++){
+        cells[i].removeEventListener('click', turnClick, false);
+    }
+    declareWinner(gameWon.player == huPlayer ? "You win!" : "You lose!")
+}
+
+function emptySquares() {
+    return origBoard.filter(s => typeof s == 'number');
+}
+
+function bestSpot() {
+    return emptySquares()[0];
+}
+
+function declareWinner(who){
+    document.querySelector(".endgame").style.display = "block";
+    document.querySelector(".endgame .text").innerText = who;
+}
+
+function checkTie() {
+    if( emptySquares().length == 0) {
+        for( var i = 0 ; i < cells.length; i++){
+            cells[i].style.backgroundColor = "green";
+            cells[i].removeEventListener('click',turnClick, false);
+        }
+        declareWinner("Tie Game!");
+        return true;
+    }
+    return false;
 }
